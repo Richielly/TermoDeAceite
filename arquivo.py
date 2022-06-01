70c732391fdcb4d75c70008fa58f827b75d1d636
Richielly <richielly.positivo@gmail.com>
2022-06-01 20:40:28 -0300

    V_1

---
+++ arquivo.py
@@ -0,0 +1,64 @@
+from docx2pdf import convert
+from docx import Document
+import pythoncom
+import win32com.client
+import time
+
+class Arquivo():
+    xl=win32com.client.Dispatch("Excel.Application",pythoncom.CoInitialize())
+
+    def alterar_doc(self, _cliente, _nrProcesso, _tipoProcesso, _anoProcesso, _contrato, _dia, _mes, _ano, _cidade, _rua, _numero, _sistema, _responsavel, _responsavelImplantacao, _responsavelAssinatura, _responsavelCargo, _responsavelNrDocumento):
+        document = Document('PROC001_PROCESSO_DE_CONVERSAO_E_IMPLANTACAO.docx')
+
+        cliente = _cliente
+        contrato = _contrato
+        dia = _dia
+        mes = _mes
+        ano = _ano
+        cidade = _cidade
+        rua = _rua
+        numero = _numero
+        sistema = _sistema
+        responsavel = _responsavel
+        tipoProcesso = _tipoProcesso
+        nrProcesso = _nrProcesso
+        anoProcesso = _anoProcesso
+        responsavelImplantacao = _responsavelImplantacao
+        responsavelAssinatura = _responsavelAssinatura
+        responsavelCargo = _responsavelCargo
+        responsavelNrDocumento = _responsavelNrDocumento
+
+        parag_3 = document.paragraphs[3]
+        parag_3.text = str(parag_3.text).replace('{$cliente}', cliente).replace('{$nrProcesso}',nrProcesso).replace('{$anoProcesso}', anoProcesso).replace('{$contrato}', contrato)
+
+        parag_8 = document.paragraphs[8]
+        parag_8.text = str(parag_8.text).replace('{$dia}',dia).replace('{$mes}',mes).replace('{$ano}',ano).replace('{$cidade}',cidade).replace('{$rua}',rua).replace('{$numero}',numero).replace('{$sistema}',sistema).replace('{$responsavel}',responsavel)
+
+        parag_9 = document.paragraphs[9]
+        parag_9.text = str(parag_9.text).replace('{$tipoProcesso}', tipoProcesso).replace('{$nrProcesso}', nrProcesso).replace('{$anoProcesso}', anoProcesso)
+
+        parag_16 = document.paragraphs[16]
+        parag_16.text = str(parag_16.text).replace('{$responsavelImplantacao}',responsavelImplantacao)
+
+        parag_18 = document.paragraphs[18]
+        parag_18.text = str(parag_18.text).replace('{$responsavelAssinatura}', responsavelAssinatura)
+
+        parag_19 = document.paragraphs[19]
+        parag_19.text = str(parag_19.text).replace('{$responsavelCargo}', responsavelCargo)
+
+        parag_20 = document.paragraphs[20]
+        parag_20.text = str(parag_20.text).replace('{$responsavelNrDocumento}', responsavelNrDocumento)
+
+        unico = (str(time.time()).replace('.',''))
+
+        document.save(f"""TermoScp_{cliente}{unico}.docx""")
+
+        return f'TermoScp_{cliente}{unico}'
+
+    def converter(self, nome):
+        xl = win32com.client.Dispatch("Excel.Application", pythoncom.CoInitialize())
+        inputFile = f"""{nome}.docx"""
+        outputFile = f"""{nome}.pdf"""
+        file = open(outputFile, "w")
+        file.close()
+        convert(inputFile, outputFile)
---
+++ main.py
@@ -0,0 +1,73 @@
+import streamlit as st
+from arquivo import Arquivo
+from PIL import Image
+
+#         sistema = 'Conabilidate Pública'
+#         responsavel = 'Marcos'
+#         tipoProcesso = 'dispensa'
+#         nrProcesso = '36'
+#         anoProcesso = '2022'
+#
+st.title("**Termo Aceite**")
+with st.form("form"):
+    st.write("**Informações da entidade**")
+    container_1 = st.container()
+    st.write("**Informações no corpo do termo**")
+    container_2 = st.container()
+    st.write("**Informações assinaturas responsável conversão/treinamento**")
+    container_3 = st.container()
+    st.write("**Informações assinaturas cliente**")
+    container_4 = st.container()
+
+    with container_1:
+        col1, col2 = st.columns(2)
+        with col1:
+            cliente = st.text_input("Nome Cliente")
+            tipoProcesso = st.text_input("Tipo Processo")
+        with col2:
+            nrProcesso = st.text_input("Número Processo")
+        with col2:
+            anoProcesso = st.text_input("Ano Processo")
+        with col2:
+            contrato  = st.text_input("Número Contrato")
+    with container_2:
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            dia = st.text_input("Dia")
+            rua = st.text_input("Rua")
+            rua = rua.upper().replace('RUA','').capitalize()
+        with col2:
+            mes = st.text_input("Mês (Ex.: 'Janeiro')")
+            numero = st.text_input("Número")
+        with col3:
+            ano = st.text_input("Ano")
+            sistema = st.text_input("Sistema")
+
+        with col4:
+            cidade = st.text_input("Cidade")
+            responsavel = st.text_input("Responsável")
+
+    with container_3:
+        col1, col2= st.columns([3,1])
+        responsavelImplantacao = st.text_input("Responsável Implantação campo assinatura")
+    with container_4:
+        col1, col2 = st.columns([3,1])
+        with col1:
+            responsavelAssinatura = st.text_input("Cliente responsável")
+        with col1:
+            responsavelCargo = st.text_input("Cargo responsável")
+            responsavelNrDocumento = st.text_input("Documento responsável")
+
+    submitted = st.form_submit_button("Enviar")
+    if submitted:
+        nome_pdf = ""
+        arq = Arquivo()
+        nome_pdf = arq.alterar_doc(cliente, nrProcesso, tipoProcesso, anoProcesso, contrato, dia, mes, ano, cidade, rua, numero, sistema, responsavel, responsavelImplantacao, responsavelAssinatura,responsavelCargo, responsavelNrDocumento)
+        arq.converter(nome_pdf)
+
+        with open(nome_pdf+'.pdf', 'rb') as f:
+            PDFbyte = f.read()
+
+if submitted:
+    st.info('Depois de clicar no botão abaixo, procure pelo arquivo "' + nome_pdf + '.pdf" na pasta de downloads.')
+    st.download_button('Download pdf', data=PDFbyte, file_name=nome_pdf + '.pdf', mime='application/octet-stream')
