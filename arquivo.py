417c531a6f797908cd4c82bc799f3c12145fdb83
Richielly <richielly.positivo@gmail.com>
2022-06-02 09:50:04 -0300

    V_19

--- arquivo.py
+++ arquivo.py
@@ -60,9 +60,9 @@ class Arquivo():
         try:
             inputFile = f"""{nome}.docx"""
             outputFile = f"""{nome}.pdf"""
-            file = open(outputFile, "w")
-            file.close()
-            msg = convert(inputFile, outputFile)
+            # file = open(outputFile, "w")
+            # file.close()
+            msg = convert(inputFile, outputFile,True)
             return msg
         except:
             return  str('Deu erro')
